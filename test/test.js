f3d9cf003c9e169af9cf172e84cbcdf6d869ca1f
RemyK <48729866+RemyK888@users.noreply.github.com>
2021-08-09 17:23:28 +0200

    Updated to Discord.js v13 !

--- test/test.js
+++ test/test.js
@@ -1,19 +1,17 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
 const { DiscordTogether } = require('../index');
 
 client.discordTogether = new DiscordTogether(client);
 
-client.on('message', async message => {
+client.on('messageCreate', async message => {
     if (message.content === 'start') {
         if(message.member.voice.channel) {
-            client.discordTogether.createTogetherCode(message.member.voice.channelID, 'youtube').then(async invite => {
+            client.discordTogether.createTogetherCode(message.member.voice.channel.id, 'youtube').then(async invite => {
                 return message.channel.send(`${invite.code}`); // Click the blue link !
             });
         };
     };
 });
 
-client.login('your Discord bot token');
-
-
+client.login('your Discord bot token here');
