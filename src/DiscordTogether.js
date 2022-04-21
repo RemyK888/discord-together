5d58837d5f65d1b0e85ca3d4a691623b755fe2fc
Adam Brangenberg <tigerbytedev@gmail.com>
2022-04-21 14:01:49 +0200

    Reviewed Acitivites and removed those which don't work
    **Working:**
    - YouTube
    - WordSnack
    - SketchHeads
    
    **Offline:**
    - YouTube Dev
    - Chess Dev
    - Doodlecrew (online but if you load it up you get a message that says you need to use SketchHeads)
    - Awkword
    - Putt Party
    - Ocho
    - Sketchy Artist
    
    **Very long loading times:**
    - Betrayal
    - Fishing
    
    **Not tested because of missing boosts:**
    - Poker
    - Chess
    - Lettertile
    - SpellCast
    - Checkers
    
    I think it's worth to consider removing the long loading ones too and add the boost ones to the docs :D

--- src/DiscordTogether.js
+++ src/DiscordTogether.js
@@ -3,22 +3,22 @@ const { Client } = require('discord.js');
 
 const defaultApplications = {
   youtube: '880218394199220334', // Note : First package to include the new YouTube Together version, any other package offering it will be clearly inspired by it
-  youtubedev: '880218832743055411', // Note : First package to include the new YouTube Together development version, any other package offering it will be clearly inspired by it
+  // youtubedev: '880218832743055411', // Note : First package to include the new YouTube Together development version, any other package offering it will be clearly inspired by it
   poker: '755827207812677713',
   betrayal: '773336526917861400',
   fishing: '814288819477020702',
   chess: '832012774040141894',
-  chessdev: '832012586023256104', // Note : First package to offer chessDev, any other package offering it will be clearly inspired by it
+  // chessdev: '832012586023256104', // Note : First package to offer chessDev, any other package offering it will be clearly inspired by it
   lettertile: '879863686565621790', // Note : First package to offer lettertile, any other package offering it will be clearly inspired by it
   wordsnack: '879863976006127627', // Note : First package to offer wordsnack any other package offering it will be clearly inspired by it
-  doodlecrew: '878067389634314250', // Note : First package to offer doodlecrew, any other package offering it will be clearly inspired by it
-  awkword: '879863881349087252', // Note : First package to offer awkword, any other package offering it will be clearly inspired by it
+  // doodlecrew: '878067389634314250', // Note : First package to offer doodlecrew, any other package offering it will be clearly inspired by it
+  // awkword: '879863881349087252', // Note : First package to offer awkword, any other package offering it will be clearly inspired by it
   spellcast: '852509694341283871', // Note : First package to offer spellcast, any other package offering it will be clearly inspired by it
   checkers: '832013003968348200', // Note : First package to offer checkers, any other package offering it will be clearly inspired by it
-  puttparty: '763133495793942528', // Note : First package to offer puttparty, any other package offering it will be clearly inspired by it
+  // puttparty: '763133495793942528', // Note : First package to offer puttparty, any other package offering it will be clearly inspired by it
   sketchheads: '902271654783242291', // Note : First package to offer sketchheads any other package offering it will be clearly inspired by it
-  ocho: '832025144389533716', // Note : First package to offer ocho any other package offering it will be clearly inspired by it
-  //sketchyartist: '879864070101172255' // Note : First package to offer sketchyartist, any other package offering it will be clearly inspired by it
+  // ocho: '832025144389533716', // Note : First package to offer ocho any other package offering it will be clearly inspired by it
+  // sketchyartist: '879864070101172255' // Note : First package to offer sketchyartist, any other package offering it will be clearly inspired by it
 };
 
 /**
